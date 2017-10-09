ce45f8aaef375ce945ec1afe841aec84885c7701
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-10-09 23:41:09 +0100

    Exercise 2

--- week3Tutorial/src/Exercise2.java
+++ week3Tutorial/src/Exercise2.java
@@ -1,4 +1,29 @@
+import java.util.Arrays;
+
 public class Exercise2 {
 
-    
+    public static void main(String[] args) {
+
+        String[] arr = {"The ", "quick ", "brown ", "fox ", "jumps ",
+            "over ", "the ", "lazy ", "dog."};
+
+        String[] newArr = remIndex(arr, 7);
+        System.out.println(Arrays.toString(newArr));
+    }
+
+    public static String[] remIndex(String[] array, int index) {
+
+        String[] newArray = new String[array.length - 1];
+
+        for (int i = 0; i < array.length; i++) {
+            if (i < index) {
+                newArray[i] = array[i];
+            } else if (i > index) {
+                newArray[i - 1] = array[i];
+            }
+        }
+
+        return newArray;
+    }
+
 }
