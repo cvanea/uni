ba844ae5a4db952a5057980b3cb4f590239f7dc1
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-10-10 23:54:34 +0100

    Pascals Triangle Generator

---
+++ week3Tutorial/src/PascalsTriangle.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PascalsTriangle {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many rows would you like for your triangle?");
+
+        printPascalTriangle(input.nextInt());
+
+    }
+
+    private static void printPascalTriangle(int numRows) {
+
+    ArrayList<ArrayList<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            triangle.add(new ArrayList<>());
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    triangle.get(i).add(1);
+                } else if (j == i) {
+                    triangle.get(i).add(1);
+                } else {
+                    triangle.get(i).add(triangle.get(i-1).get(j-1) + triangle.get(i-1).get(j));
+                }
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < triangle.size(); i++) {
+            for (int j = triangle.size(); j >= i; j--) {
+                stringBuilder.append(" ");
+            }
+            stringBuilder.append(triangle.get(i)).append("\n");
+        }
+
+        String finalTriangle = stringBuilder.toString().replace(",", "")
+            .replace("[","").replace("]", "");
+
+        System.out.println(finalTriangle);
+
+    }
+
+}
