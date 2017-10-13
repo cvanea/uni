6c1461595536d83477ffc2afdc66afb0da2a9093
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-10-13 16:14:07 +0100

    Fully finished exercise 4

--- week3Tutorial/src/Exercise4.java
+++ week3Tutorial/src/Exercise4.java
@@ -1,6 +1,7 @@
 import java.util.Collections;
 import java.util.Scanner;
 import java.util.Vector;
+import java.util.ArrayList;
 
 public class Exercise4 {
 
@@ -10,16 +11,23 @@ public class Exercise4 {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What are your real numbers? Please separate by ','");
+        System.out.println("What are your real numbers? Please separate numbers by a space and input 0 to exit");
 
-        String[] numbers = input.nextLine().split(", ");
 
-        for (String number : numbers) {
+        ArrayList<String> numbers2 = new ArrayList<>();
+
+        while (input.hasNext() && !input.hasNext("0") && numbers2.size() < 50) {
+                numbers2.add(input.next());
+        }
+
+        for (String number : numbers2) {
             sortReal.add(Double.valueOf(number));
         }
 
         Collections.sort(sortReal);
 
+        System.out.println(numbers2);
+        System.out.println(numbers2.size());
         System.out.println(sortReal);
     }
 }
