93544a014102f7e1798d6786469bfcc4be6e7035
Claudia Vanea <claudia.vanea@hotmail.co.uk>
2017-11-03 17:20:35 +0000

    Finished exercise 2

--- week6Tutorial/src/exercise2/guide2/Controller.java
+++ week6Tutorial/src/exercise2/guide2/Controller.java
@@ -1,4 +1,22 @@
 package exercise2.guide2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.Random;
+
 public class Controller {
+
+    @FXML
+    private Label myMessage;
+
+    public void generateRandom(ActionEvent event) {
+        Random rand = new Random();
+        int myRand = rand.nextInt(50) + 1;
+
+        myMessage.setText(Integer.toString(myRand));
+
+//        System.out.println(Integer.toString(myRand));
+    }
 }
